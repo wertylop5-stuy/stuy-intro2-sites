83f23e87feb58d3126eefccc6107713be1622c63
Stanley Lin <slin55055@gmail.com>
2016-04-06 20:21:59 -0400

    ugh

--- labs/lab08/lab08.py
+++ labs/lab08/lab08.py
@@ -6,7 +6,7 @@ import random
 def startPage(title):
 	return "<!DOCTYPE html>\n<html>\n" + makeTabs(1) + \
 	"<head>\n" + makeTabs(2) + "<title>" + title + "</title>\n" + \
-	makeTabs(2) + "<link rel='stylesheet' href='pretty.css'></link>" + \
+	makeTabs(2) + "<link rel='stylesheet' href='pretty.css'>\n" + \
 	makeTabs(1) + "</head>\n" + makeTabs(1) + "<body>"
 
 def endPage():
