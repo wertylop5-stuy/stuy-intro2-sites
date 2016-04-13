cb0f1b71046384583034854d1963f5e1c4c855ce
Stanley Lin <slin55055@gmail.com>
2016-04-12 23:28:52 -0400

    Done with lab9

--- labs/lab09/MadLib.py
+++ labs/lab09/MadLib.py
@@ -30,27 +30,49 @@ def replaceSpeechPart(target, partOfSpeech, dataArray):
 	prevValues = []
 	temp = ""
 	temp2 = ""
+	#the 1 in VERB1
+	tempNum = 0
+	
 	
 	hit = result.find(partOfSpeech)
 	while hit != -1:
-		if not(result[hit:] != "") and \
-		result[hit + len(partOfSpeech)].isDigit():
-			temp = result[hit:hit + len(partOfSpeech)]
-			if not(temp in prevValues):
-				prevValues.append(temp)
-				temp2 = dataArray[random.randint(0, \
-				len(dataArray) - 1)]
+		#if not end of string
+		if result[hit:] != "":
+			if result[hit + len(partOfSpeech)].isdigit():
+				#temp takes value of whole hit including end number
+				temp = result[hit:hit + len(partOfSpeech) + 1]
+				if not(temp in prevValues):
+					prevValues.append(temp)
+					temp2 = dataArray[random.randint(0, \
+					len(dataArray) - 1)]
+					
+					prevFind.append(temp2)
+					
+					#yes, this is destructive. probs a bad idea
+					dataArray.remove(temp2)
 				
-				prevFind.append(temp2)
+				#only the end number of the hit
+				tempNum = result[hit + len(partOfSpeech)]
 				
+				result = result.replace(partOfSpeech + tempNum, prevFind[int(tempNum) - 1])
+			
+			#could refactor this, but...
 			else:
+				temp = dataArray[random.randint(0, len(dataArray) - 1)]
+				#dangerous
+				dataArray.remove(temp)
 				
+				result = result[:hit] + temp + \
+				result[hit + len(partOfSpeech):]
+				hit = result.find(partOfSpeech)
 		else:
 			temp = dataArray[random.randint(0, len(dataArray) - 1)]
+			#dangerous
+			dataArray.remove(temp)
+			
 			result = result[:hit] + temp + \
 			result[hit + len(partOfSpeech):]
-			#print res
-			hit = result.find(partOfSpeech)
+		hit = result.find(partOfSpeech)
 	return result
 	
 def madlibs(storyString, nounList, verbList, adjectiveList, \
@@ -94,13 +116,55 @@ def makeTabs(num):
 
 print startPage("STORIES!!!!")
 
-storyString = madlibs("NOUN VERB, ADJECTIVE:; PROPERnOUN []r ADvERB", \
+'''storyString = madlibs("NOUN VERB, ADJECTIVE:; PROPERnOUN []r ADvERB", \
 ["pizza", "donut"], ["running", "walking"], ["green", "yellow"], \
-["Rick", "David"], ["quickly", "slowly"])
+["Rick", "David"], ["quickly", "slowly"])'''
+'''storyResult = madlibs("""
+NOUN1 NOUN2, NOUN1 NOUN2.
+VERB1 VERB1 VERB VERB1 VERB2 VERB2
+ADJECTIVE1 ADJECTIVE ADJECTIVE2 ADJECTIVE1
+PROPERnOUN PROPERnOUN1 PROPERnOUN1 PROPERnOUN2
+""", \
+["pizza", "donut", "cactus"], ["running", "walking", "swimming"], ["red", "green", "yellow"], \
+["Rick", "David", "Morty"], ["heavenly", "quickly", "slowly"])'''
+
+storyRoll = random.randint(0, 1)
+storyString = ""
+bbb = ""
+
+if storyRoll == 1:
+	bbb = "Romeo and Juliet"
+	storyString = '''
+But soft, what NOUN1 through yonder NOUN breaks?
+It is the east and PROPERnOUN is the NOUN2!
+Arise, ADJECTIVE1 NOUN2, and kill the envious NOUN,
+Who is already VERB1 and pale with grief
+That thou her NOUN3 art far more ADJECTIVE1 than she.
+Be not her NOUN3, since she is envious;
+Her NOUN4 livery is but VERB1 and green,
+And none but fools do wear it. Cast it off.
+It is my lady, O, it is my love!
+O that she knew she were!
+'''
+else:
+	bbb = "Wuthering Heights"
+	storyString = '''
+About twelve o'clock that NOUN was born the PROPERnOUN you saw at ADvERB1 NOUN1: a ADJECTIVE1, seven-months' child; and two hours after the mother died, having never VERB1 ADJECTIVE1 consciousness to VERB2 PROPERnOUN1, or VERB1 PROPERnOUN1. The latter's distraction at his bereavement is a subject too painful to be VERB3 on; its after-effects showed how ADJECTIVE2 the sorrow VERB3. A great addition, in my eyes, was his being left without an heir. I bemoaned that, as I gazed on the ADJECTIVE2 orphan; and I mentally VERB4 old PROPERnOUN1 for (what was only ADJECTIVE3 partiality) the VERB4 his estate to his own daughter, instead of his son's.
+'''
+
+noun = ["laser gun", "lightsaber", "wormhole", "infinite improbability drive", "portal", \
+"space-time continuum", "starship"]
+adj = ["interstellar", "quantum", "universal", "otherworldy", "galactic"]
+verb = ["fly", "blast", "warp", "teleport"]
+proper = ["Ender", "Ford Prefect", "Obi Wan Kenobi", "Bugger"]
+adverb =["silently", "slowly"]
+
+storyResult = madlibs(storyString, noun, verb, adj, proper, adverb)
 
 print makeTabs(2) + "<h1>" + "STORY TIMMMMEEEE!!!!!1!1" + "</h1>"
+print makeTabs(2) + "<h3>" + bbb + "!!!!!1!1" + "</h3>"
 
-print makeTabs(2) + "<p id='storyText'>" + storyString + "</p>"
+print makeTabs(2) + "<p id='storyText'>" + storyResult + "</p>"
 
 
 print endPage()
