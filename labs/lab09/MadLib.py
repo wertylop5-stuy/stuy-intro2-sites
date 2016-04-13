f4f62aee7f1fffe5a2ce25de0b58d7c05b80e390
Stanley Lin <slin55055@gmail.com>
2016-04-12 22:02:32 -0400

    weg

--- labs/lab09/MadLib.py
+++ labs/lab09/MadLib.py
@@ -1,45 +1,115 @@
+#!/usr/bin/python
+print "content-type: text/html\n"
+#TODO finish up the VERB1 detection stuff
 import random
-
-def replaceSpeechPart(target, partOfSpeech, dataArray):
-	result = target
-	hit = result.find(partOfSpeech)
-	temp = ""
-	while hit != -1:
-		temp = dataArray[random.randint(0, len(dataArray) - 1)]
-		result = result[:hit] + temp + result[hit + len(partOfSpeech):]
-		#print res
-		hit = result.find(partOfSpeech)
-	return result
-
 
 #storyString will have NOUN, ADJ, etc. scattered
 #find the first instance of one of these
 	#and match it to the coresponding word array
+
 #return a string with the characters before the
 	#hit appended with a random word, then the 
 	#rest of the string appended
-def madlibs(storyString, nounList, verbList, adjectiveList):
+
+#handling speechParts with numbers (lowest number is 1)
+	#check if the hit is the last word in the string
+		#if it is, normal execution
+	
+	#else, if the character right after the hit is
+		#a number, search an array of previous hits
+			#if not initially there, append it and move on
+			#if exists, take the value at that index and 
+			#replace the hit with ie
+def replaceSpeechPart(target, partOfSpeech, dataArray):
+	result = target
+	
+	#handles speechParts with numbers'
+	#holds the randomly assigned values
+	prevFind = []
+	#holds the previously found occurences (ex VERB1)
+	prevValues = []
+	temp = ""
+	temp2 = ""
+	
+	hit = result.find(partOfSpeech)
+	while hit != -1:
+		if not(result[hit:] != "") and \
+		result[hit + len(partOfSpeech)].isDigit():
+			temp = result[hit:hit + len(partOfSpeech)]
+			if not(temp in prevValues):
+				prevValues.append(temp)
+				temp2 = dataArray[random.randint(0, \
+				len(dataArray) - 1)]
+				
+				prevFind.append(temp2)
+				
+			else:
+				
+		else:
+			temp = dataArray[random.randint(0, len(dataArray) - 1)]
+			result = result[:hit] + temp + \
+			result[hit + len(partOfSpeech):]
+			#print res
+			hit = result.find(partOfSpeech)
+	return result
+	
+def madlibs(storyString, nounList, verbList, adjectiveList, \
+properNounList, adverbList):
 	#final string to return
 	res = storyString
 	hit = 0
 	res = replaceSpeechPart(res, "NOUN", nounList)
 	res = replaceSpeechPart(res, "VERB", verbList)
 	res = replaceSpeechPart(res, "ADJECTIVE", adjectiveList)
+	res = replaceSpeechPart(res, "PROPERnOUN", properNounList)
+	res = replaceSpeechPart(res, "ADvERB", adverbList)
 	return res
 
 
 
-print madlibs("hello NOUN NOUN NON VERB ADJ ECTIVE ADJECTIVE VERB hu ADJECTIVE", \
-["operand", "yu"], ["running", "walking"], ["green", "smelly"])
+'''print madlibs("hello NOUN NOUN NON VERB ADJ ECTIVE ADJECTIVE VERB hu ADJECTIVE", \
+["operand", "yu"], ["running", "walking"], ["green", "smelly"])'''
 
-print madlibs('''To VERB, or not to VERB--that is the NOUN:
-Whether 'tis nobler in the NOUN to VERB
-The NOUNs and NOUNs of ADJECTIVE fortune
-Or to take NOUNs against a sea of NOUNs
-And by opposing end them. To VERB, to VERB--
-No more--and by a VERB to say we end
-The heartache, and the thousand ADJECTIVE VERB
-That flesh is heir to. ''', \
-['fish','computer','Iphone','jaguar','mapo tofu'], \
-['run','spin','talk','hit','jig','shave','cut','rap'], \
-['silent','big','small','hairy','rich','amazing','outrageous','preposterous'])
+'''print madlibs("NOUN VERB, ADJECTIVE:; PROPERnOUN []r ADvERB", \
+["pizza", "donut"], ["running", "walking"], ["green", "yellow"], \
+["Rick", "David"], ["quickly", "slowly"])'''
+
+
+################### BEGIN WEBSITE ###################
+def startPage(title):
+	return "<!DOCTYPE html>\n<html>\n" + makeTabs(1) + \
+	"<head>\n" + makeTabs(2) + "<title>" + title + "</title>\n" + \
+	makeTabs(2) + "<link rel='stylesheet' href='pretty.css'>\n" + \
+	makeTabs(1) + "</head>\n" + makeTabs(1) + "<body>"
+
+def endPage():
+	return makeTabs(1) + "</body>\n</html>"
+
+def makeTabs(num):
+	result = ""
+	for x in range(0, num):
+		result += "    "
+	return result
+
+
+print startPage("STORIES!!!!")
+
+storyString = madlibs("NOUN VERB, ADJECTIVE:; PROPERnOUN []r ADvERB", \
+["pizza", "donut"], ["running", "walking"], ["green", "yellow"], \
+["Rick", "David"], ["quickly", "slowly"])
+
+print makeTabs(2) + "<h1>" + "STORY TIMMMMEEEE!!!!!1!1" + "</h1>"
+
+print makeTabs(2) + "<p id='storyText'>" + storyString + "</p>"
+
+
+print endPage()
+
+
+
+
+
+
+
+
+
