2bb0ac6fd535208b9598a5a1bd04f693a4b278c3
Stanley Lin <slin55055@gmail.com>
2016-04-21 16:15:24 -0400

    fwewg

--- labs/lab11/results.py
+++ labs/lab11/results.py
@@ -4,8 +4,45 @@ import sys
 sys.path.insert(0, "../modules")
 from htmlFuncts import *
 
+import cgitb
+cgitb.enable()
+
+import cgi
+
+form = cgi.FieldStorage()
+#print form
+
 print startPage("Results")
 
+if "oneLiner" in form:
+	print makeTabs(2) + "<p>" + "Hey " + \
+	form.getvalue("oneLiner").capitalize() + \
+	"</p>"
+else:
+	print "Please fill out the hey field"
+	sys.exit(0)
+
+if "clicky" in form and "clicky2" in form:
+	print "clickies~!!!!!<br>"
+elif "clicky" in form or "clicky2" in form:
+	print "Only one clicky<br>"
+else:
+	print "Y U no like clickies?<br>"
+
+if "grade" in form:
+	print "you are in grade " + form.getvalue("grade") +\
+	"<br>"
+if form.getvalue("grade") == "9":
+	print "freshie ;)" + "<br>"
+
+if "dude" in form:
+	print "That's so " + form.getvalue("dude")
+else:
+	print "Indicate if Fresh or rad?<br>"
+	sys.exit(0)
+
+if "essay" in form:
+	print "you like writing huh?<br>"
 
 
 print endPage()
