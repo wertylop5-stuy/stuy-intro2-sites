a73dd4ca5569419c4e627618e549d2011d654e1a
Stanley Lin <slin55055@gmail.com>
2016-04-24 12:22:32 -0400

    t

--- labs/lab12/lab12.py
+++ labs/lab12/lab12.py
@@ -16,7 +16,7 @@ def searchByName(L, name, searchCol):
 	for inner in L:
 		if not lel:
 			lel = True
-			res.appen(inner)
+			res.append(inner)
 			continue
 		if name in inner[searchCol].lower():
 			res.append(inner)
