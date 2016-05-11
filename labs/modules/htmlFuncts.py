604a08d6a8e2d2b9057a22e0b3d3f6eac857770c
Stanley Lin <slin55055@gmail.com>
2016-05-11 19:53:40 -0400

    fwe

--- labs/modules/htmlFuncts.py
+++ labs/modules/htmlFuncts.py
@@ -7,7 +7,7 @@ def startPage(title):
 def startPageN(title):
 	return "<!DOCTYPE html>\n<html>\n" + makeTabs(1) + \
 	"<head>\n" + makeTabs(2) + "<title>" + title + "</title>\n" + \
-	makeTabs(2) + "<link rel='stylesheet' href='pretty.css'>\n" + \
+	makeTabs(2) + "<link rel='stylesheet' href='stylesheet.css'>\n" + \
 	makeTabs(1) + "</head>\n" + makeTabs(1) + "<body>"
 
 def endPage():
