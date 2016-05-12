3602d2928196d9da0933b5f547772a528c219ae3
Stanley Lin <slin55055@gmail.com>
2016-05-12 17:54:53 -0400

    f

--- labs/lab15/createaccount.py
+++ labs/lab15/createaccount.py
@@ -28,6 +28,30 @@ def dataWipe(direct, fileN):
 	temp.close()
 	print "Wipe successful"
 
+def getUsersFromFile(fileN):
+	strippedData = fileN.split("\n")
+	#removes the rest of the string after comma
+	for index, value in enumerate(strippedData):
+		strippedData[index] = value[:value.find(",")]
+	strippedData.pop()
+	return strippedData
+
+#split form by comma
+#if newline in element, pop it
+#if username in resulting list, ignore
+#else, add it
+def addUser(form, directory, fileN, user, passW):
+	stream = None
+	userList = getUsersFromFile(form)
+	if not(user in userList):
+		stream = open(directory + fileN, "a")
+		stream.write(
+			form.getvalue("username") + "," +
+			form.getvalue("pass") + "\n"
+			)
+		stream.close()
+
+
 if "done" in form:
 	direct = "data/"
 	data = "usernames.txt"
