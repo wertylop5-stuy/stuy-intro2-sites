17e796ca77e1f86d27f84442806e000f4f4c139b
Stanley Lin <slin55055@gmail.com>
2016-05-26 22:04:06 -0400

    Fwe

--- labs/finalproject/0526/postExpanded.py
+++ labs/finalproject/0526/postExpanded.py
@@ -30,15 +30,6 @@ form = cgi.FieldStorage()
 
 
 
-
-
-
-
-
-
-
-
-
 #gordons code
 def poster():
 	return '''<br>
@@ -51,76 +42,43 @@ Text: <textarea name="comment" rows="10" cols="15">
 <br>
 <input type = "submit" value = "Make comment">
 </form>'''
-'''
-def displayPost(titleTag, bodyTag, userTag, commentTag=""):
-	postStream = open(stdStuff.directory + stdStuff.postFile, "r")
-	allPosts = postStream.read()
-	postStream.close()
 
-	listOfPosts = allPosts.split(stdStuff.splitPost)
-	listOfPosts.pop()
-	
-	postResult = ""
-	if len(allPosts) > 0:
-			for post in listOfPosts:
-				listTemp = post.split(stdStuff.splitChar)
-				
-				if not(postID == listTemp[0]):
-					continue
-				postResult += stdStuff.makeTag(userTag, listTemp[0])
-				postResult += stdStuff.makeTag(userTag, listTemp[1])
-				postResult += stdStuff.makeTag(titleTag, listTemp[2])
-				postResult += stdStuff.makeTag(bodyTag, listTemp[3])
-				
-				return postResult
-'''
-def displayPost(postObj, titleTag, bodyTag, userTag, commentTag=""):
+def displayPost(postObj, titleTag, bodyTag, userTag, commentTag):
 	postResult = ""
 	postResult += 	stdStuff.makeTag(userTag, postObj.id) + \
 					stdStuff.makeTag(userTag, postObj.user) + \
 					stdStuff.makeTag(titleTag, postObj.title) + \
 					stdStuff.makeTag(bodyTag, postObj.text)
 	
+	postResult += displayComments(postObj.comments, commentTag)
+	
 	postResult += '''<br>
 <br>
 <br>
 <br>
-<form action = "profile.py" method = "GET">
+<form action = "postExpanded.py" method = "GET">
 Text: <textarea name="comment" rows="10" cols="15">
 </textarea>
 <br>
-<input type = "submit" value = "Make comment">
+<input type = "submit" name="done" value = "Make comment">
 </form>'''
 	
 	return postResult
 
-def getIndexOfID(L, idNum):
-	'''get the index in a list '''
+def displayComments(commentList, userTag, bodyTag):
+	res = ""
+	for comment in commentList:
+		res += 	stdStuff.makeTag(userTag, comment.user) + \
+				stdStuff.makeTag(bodyTag, comment.text)
+	return res
 
-def writeComment(commentText, c):
-	"""writes comment to file"""
-	commentStream = open(stdStuff.directory + stdStuff.commentFile, "r")
-	commentString = commentStream.read()
-	commentStream.close()
-	
-	listOfComments = commentString.split(stdStuff.postChar)
-	for index, value in enumerate(listOfComments):
-		g[index] = value.split(".")
-		while "" in g[index]:
-			g[index].remove("")
-
-	
-	
-
-def displayComments():
-	commentStream = open(stdStuff.directory + stdStuff.commentFile, "r")
-	commentString = commentStream.read()
-	commentStream.close()
-	
-	#never changes
-	listOfComments = tuple(commentString.split(stdStuff.postChar))
-	
-	
+def writeComment(commentText, cookie, targId):
+	allPosts = stdStuff.objFileToList(stdStuff.directory,
+										 stdStuff.postFile)
+	for index, value in enumerate(allPosts):
+		if value.id == targId:
+			allPosts[index].addComment(cookie['username'].value,
+										commentText)
 
 def authenticate(u,ID,IP):
     loggedIn = open(stdStuff.directory + stdStuff.logFile,'r').read().split('\n')
