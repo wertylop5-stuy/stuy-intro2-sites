b910638d2db36f777f9cde06b671407634ce18c4
Stanley Lin <slin55055@gmail.com>
2016-05-27 21:38:03 -0400

    Fwe

--- labs/finalproject/0527/profile.py
+++ labs/finalproject/0527/profile.py
@@ -144,7 +144,7 @@ if 'HTTP_COOKIE' in os.environ:
 """
 			allPosts = stdStuff.objFileToList(stdStuff.directory,
 										stdStuff.postFile)
-			"""if "downVote" in form or "upVote" in form:
+			if "downVote" in form or "upVote" in form:
 				targId = form.getvalue("postId")
 				'''
 				if "downVote" in form:
@@ -160,7 +160,7 @@ if 'HTTP_COOKIE' in os.environ:
 							allPosts[index].score += 1
 							break
 				'''
-				if "downVote" in form:
+				'''if "downVote" in form:
 					for index, value in enumerate(allPosts):
 						if value.id == targId:
 							allPosts[index].score -= 1
@@ -174,7 +174,7 @@ if 'HTTP_COOKIE' in os.environ:
 															stdStuff.postFile)
 							allPosts = stdStuff.objFileToList(stdStuff.directory,
 										stdStuff.postFile)
-				"""
+				'''
 			
 			### PUT PAGE STUFF HERE
 			if "postTitle" in form:
