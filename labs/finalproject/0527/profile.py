08401de09423c502b9c55b0e70198c6092706655
Stanley Lin <slin55055@gmail.com>
2016-05-27 21:03:14 -0400

    fw

--- labs/finalproject/0527/profile.py
+++ labs/finalproject/0527/profile.py
@@ -138,7 +138,7 @@ def makePage():
 	return res
 
 
-
+'''
 if 'HTTP_COOKIE' in os.environ:
 	cookie_string=os.environ.get('HTTP_COOKIE')
 	c = Cookie.SimpleCookie()
@@ -197,11 +197,42 @@ if 'HTTP_COOKIE' in os.environ:
 else:
 	body+= 'You seem new<br>\n'
 	body+='Go Login <a href="login.py">here</a><br>'
-
+'''
 
 print 'content-type: text/html'
 print ''
 
+
+
 print head
+
+body += """<form method="GET" action="homepage.py">
+<input name="logOut" type="submit" value="Log out">
+</form>
+"""
+allPosts = stdStuff.objFileToList(stdStuff.directory,
+							stdStuff.postFile)
+print form
+if "downVote" in form or "upVote" in form:
+	targId = form.getvalue("postId")
+	
+	if "downVote" in form:
+		for index, value in enumerate(allPosts):
+			if value.id == targId:
+				print allPosts[index].score
+				allPosts[index].score -= 1
+				print allPosts[index].score
+				break
+	elif "upVote" in form:
+		for index, value in enumerate(allPosts):
+			if value.id == targId:
+				allPosts[index].score += 1
+				break
+	stdStuff.objListToFile(allPosts, stdStuff.directory, 
+									stdStuff.postFile)
+	allPosts = stdStuff.objFileToList(stdStuff.directory,
+							stdStuff.postFile)
+				
+			
 print body
 print foot
