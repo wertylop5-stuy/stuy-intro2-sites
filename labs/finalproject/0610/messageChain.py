42dcf1a26741375b9b1a0d926e473fe642d998c6
Stanley Lin <slin55055@gmail.com>
2016-06-10 22:32:54 -0400

    Merging with Gordon

--- labs/finalproject/0610/messageChain.py
+++ labs/finalproject/0610/messageChain.py
@@ -132,6 +132,7 @@ if 'HTTP_COOKIE' in os.environ:
 <input name="addFriend" type="submit" value="Add a friend">
 </form>
 <a href="profile.py">Go back to profile</a>
+<a href="inbox.py">Go back to inbox</a>
 """
 			body += makePage(c)
 			
