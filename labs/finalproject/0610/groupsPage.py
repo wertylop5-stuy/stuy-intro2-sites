351bfb9bfca3fcbff3608792b5a8142346ec56de
Stanley Lin <slin55055@gmail.com>
2016-06-11 00:09:56 -0400

    Merging with GordonA

--- labs/finalproject/0610/groupsPage.py
+++ labs/finalproject/0610/groupsPage.py
@@ -400,7 +400,7 @@ if 'HTTP_COOKIE' in os.environ:
 			
 			name = groupDict[currentGroup]
 			currentUser = c["username"].value
-			if ("downVote" in form) or ("upVote" in form) or
+			if ("downVote" in form) or ("upVote" in form) or \
 			("removeVote" in form):
 				targId = int(form.getvalue("postId"))
 				if "downVote" in form:
